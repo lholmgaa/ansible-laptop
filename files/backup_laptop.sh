f5bf6eac6d78ec255c0680c4148ec5b87a0820cc
Lars Holmgaard <lars.holmgaard@stilk.dk>
2025-12-21 18:10:43 +0100

    Only run backup on home network

--- files/backup_laptop.sh
+++ files/backup_laptop.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# 1. Quietly check if the backup server is reachable
+# -c 1: send 1 packet
+# -W 2: wait 2 seconds for a response
+# &> /dev/null: hide all output
+if ! ping -c 1 -W 2 backup &> /dev/null; then
+    exit 0
+fi
+
+# 2. Copy the fstab file
 cp /etc/fstab /home/lars/bin
-rsync -azP --delete --exclude "*Downloads*" --exclude "*VirtualBox*" --exclude "*vagrant*" --exclude 'stil' --exclude "*node_modules*" --exclude ".gradle" --exclude ".class" --exclude ".cache" --exclude ".m2" --exclude ".config" --exclude ".npm" --exclude ".vscode" --exclude "VirtualBox VMs" --exclude ".local" --exclude ".cache" /home/lars lholmgaa@backup:/mnt/usb/lars_home_laptop
-~      
+
+# 3. Run the rsync backup
+rsync -azP --delete \
+  --exclude "*Downloads*" \
+  --exclude "*VirtualBox*" \
+  --exclude "*vagrant*" \
+  --exclude 'stil' \
+  --exclude "*node_modules*" \
+  --exclude ".gradle" \
+  --exclude ".class" \
+  --exclude ".cache" \
+  --exclude ".m2" \
+  --exclude ".config" \
+  --exclude ".npm" \
+  --exclude ".vscode" \
+  --exclude "VirtualBox VMs" \
+  --exclude ".local" \
+  /home/lars lholmgaa@backup:/mnt/usb/lars_home_laptop2
